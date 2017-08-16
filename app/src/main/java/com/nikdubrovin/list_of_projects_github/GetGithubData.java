77cef7b07647c5e0be270e6e709662f51a623948
NIkDubrovin <NikDresden@outlook.com>
2017-08-16 18:01:26 +0300

    Initial commit.

---
+++ app/src/main/java/com/nikdubrovin/list_of_projects_github/GetGithubData.java
@@ -0,0 +1,98 @@
+package com.nikdubrovin.list_of_projects_github;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.R.attr.country;
+import static android.R.attr.description;
+import static android.R.attr.name;
+
+
+/**
+ * Created by NikDubrovin on 16.08.2017.
+ */
+
+public class GetGithubData extends AsyncTask<String,Void,JSONObject> {
+
+    private final String TAG = "GitHubAPI";
+    private final String URL_API = "https://api.github.com/users/";
+    private URL url;
+    private String User;
+    private String Data;
+   // https://api.github.com/users/yatingupta10/repos - список репозиториев с данными
+
+
+    @Override
+    protected JSONObject doInBackground(String... strings) {
+        JSONObject jsonReturnFile = new JSONObject();
+
+        try{
+
+            //region Получаем  Json-строку( виде String) от сервера при помощи Http запроса
+
+            String url_str = URL_API +  User + "/" + Data;
+
+            if(validateUrl(url_str)) // Проверка - является ли строка url-адресом
+                url = new URL(url_str);
+            else
+            Log.i(TAG,"Failed URL!");
+
+            Log.i(TAG,url_str);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            if (connection == null)
+                Log.i(TAG,"Failed to make connection!");
+            else
+                Log.i(TAG,"Successfully to make connection!");
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+          //  Log.i(TAG, "Json File: " + response.toString()); -  Skipped 105 frames!  The application may be doing too much work on its main thread.
+
+            //endregion Получение строки закончили
+
+            JSONArray json = new JSONArray(response.toString()); // Превращаем String in JSONObject
+            for (int i =0;i<json.length();i++) {
+                String name = json.getJSONObject(0).getString("name");
+                Log.i(TAG, "NAME: " + name);
+                String lang = json.getJSONObject(0).getString("language");
+                Log.i(TAG, "Language: " + lang);
+                String url_repos = json.getJSONObject(0).getString("html_url");
+                Log.i(TAG, "URL_REPOS: " + url_repos);
+                String description = json.getJSONObject(0).getString("description");
+                Log.i(TAG, "Description: " + description);
+
+                jsonReturnFile.put("name" + i, name).put("url_repos" + i, url_repos).put("description" + i, description).put("language" + i, lang);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        catch (Throwable cause){
+            cause.printStackTrace();
+        }
+        Log.i(TAG, "jsonReturn: " + jsonReturnFile);
+        return jsonReturnFile;
+    }
+
+    public boolean validateUrl(String adress){
+        return android.util.Patterns.WEB_URL.matcher(adress).matches();
+    }
+
+    public void setUser(String User) {this.User = User;}
+    public void setData(String Data) {this.Data = Data;}
+}
---
+++ app/src/main/java/com/nikdubrovin/list_of_projects_github/MainActivity.java
@@ -0,0 +1,90 @@
+package com.nikdubrovin.list_of_projects_github;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GetGithubData getGithubData;
+    private final String TAG = "MainActivity";
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+    //region Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()){
+            case R.id.action_settings :
+                return true;
+            case R.id.about_app :
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("About")
+                        .setMessage("by @NIkDubrovin");
+                AlertDialog alert = builder.create();
+                alert.show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickGetData(View view) {
+        editText = (EditText)findViewById(R.id.EditText_CityName);
+        String name_login = editText.getText().toString();
+        getGithubData = new GetGithubData();
+        getGithubData.setUser(name_login);
+        getGithubData.setData("repos");
+        getGithubData.execute();
+        try {
+            JSONObject result = getGithubData.get();
+            Log.i(TAG, "result: " + result);
+            String name = result.getString("name");
+            URL url = new URL(result.getString("url_repos"));
+            String desc = result.getString("description");
+            String lang = result.getString("language");
+
+            Toast.makeText(getApplicationContext(),
+                    "Репозиторий : " + name + "\n" +"URL: " + url + "\n" +
+                            "Описание: " + desc + "\n" +
+                            "Язык: " + lang, Toast.LENGTH_LONG).show();
+        }catch (Throwable cause){
+            cause.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Что-то пошло не так..", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //endregion
+}
